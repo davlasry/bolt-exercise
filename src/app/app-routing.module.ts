cc5e355b7305671c6f737c8c18f885df9bcfa18d
David Lasry <david.l@tomadox.co.uk>
2019-07-24 14:48:56 +0300

    single-product layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductsComponent } from './components/products/products.component';
+import { ListComponent } from './components/list/list.component';
+import { ThumbComponent } from './components/thumb/thumb.component';
+import { SingleProductComponent } from './components/single-product/single-product.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: 'products', component: ProductsComponent }
+  { path: '', redirectTo: '/products/list', pathMatch: 'full' },
+  {
+    path: 'products',
+    component: ProductsComponent,
+    children: [
+      { path: '', redirectTo: 'list', pathMatch: 'full' },
+      { path: 'list', component: ListComponent },
+      { path: 'thumb', component: ThumbComponent }
+    ]
+  },
+  {
+    path: 'single-product',
+    component: SingleProductComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
+import { ListComponent } from './components/list/list.component';
+import { ThumbComponent } from './components/thumb/thumb.component';
+import { SingleProductComponent } from './components/single-product/single-product.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent],
+  declarations: [AppComponent, ProductsComponent, ListComponent, ThumbComponent, SingleProductComponent],
   imports: [BrowserModule, AppRoutingModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent]
