fc27b6bf81cf89a9bb07ab9999335f3ebdf1626e
David Lasry <david.l@tomadox.co.uk>
2019-07-25 14:42:43 +0300

    finish version

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
+  images = [
+    { id: 1, url: 'assets/images/product_image1.png' },
+    { id: 2, url: 'assets/images/product_image2.png' },
+    { id: 3, url: 'assets/images/product_image3.png' },
+    { id: 4, url: 'assets/images/product_image4.png' }
+  ];
+
+  activeImageUrl = 'assets/images/product_image1.png';
+
   constructor() {}
 
-  ngOnInit() {}
+  onClickPicture(imageUrl) {
+    this.activeImageUrl = imageUrl;
+  }
 }
--- src/app/components/single-product/single-product.component.ts
+++ src/app/components/single-product/single-product.component.ts
@@ -5,11 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './single-product.component.html',
   styleUrls: ['./single-product.component.scss']
 })
-export class SingleProductComponent implements OnInit {
+export class SingleProductComponent {
+  images = [
+    { id: 1, url: 'assets/images/product_image1.png' },
+    { id: 2, url: 'assets/images/product_image2.png' },
+    { id: 3, url: 'assets/images/product_image3.png' },
+    { id: 4, url: 'assets/images/product_image4.png' }
+  ];
 
-  constructor() { }
+  activeImageUrl = 'assets/images/product_image1.png';
 
-  ngOnInit() {
+  constructor() {}
+
+  onClickPicture(imageUrl) {
+    this.activeImageUrl = imageUrl;
   }
-
 }
